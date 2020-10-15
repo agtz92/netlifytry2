8fa46b94e9087d341af5d1eaaf995fecf1b97be7
agtz92 <alfredo.gtz92@gmail.com>
2020-10-15 03:54:52 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,23 @@ import { css } from "@emotion/core"
 export default function Home({ data }) {
   console.log(data)
   return (
-    <div>
-      <h1>
-        No se como vergas corregirlo
+      <div>
+        <h1>
+          Amazing Pandas Eating Things
         </h1>
-      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <h3            >
-            {node.frontmatter.title}{" "}
-            <span
-            >
-              — {node.frontmatter.date}
-            </span>
-          </h3>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
-    </div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
   )
 }
 
