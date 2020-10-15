3508af491b3916b782f66494e5a1b922234eb2ff
agtz92 <alfredo.gtz92@gmail.com>
2020-10-15 03:51:28 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,45 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  console.log(data)
+  return (
+    <div>
+      <h1>
+        No se como vergas corregirlo
+        </h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3            >
+            {node.frontmatter.title}{" "}
+            <span
+            >
+              — {node.frontmatter.date}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
